e3d83bfeb2c46512a995f3af22589329c3b26dec
Bambang Marsudi <bambang.marsudi.ft18@mail.umy.ac.id>
2021-01-14 14:23:03 +0700

    Add project files.

---
+++ ConsoleApp1ServerChatApp_113_bambang/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_201880140113;
+
+namespace ConsoleApp1ServerChatApp_113_bambang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
